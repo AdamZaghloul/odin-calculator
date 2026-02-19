5129fb477e8b307818137423dfcf6d7dc40e60e0
AdamZaghloul <adam.zaghloul95@gmail.com>
2026-02-18 20:39:49 -0500

    rest of calculator functions.

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-let a = 0;
-let b = 0;
+let a = null;
+let b = null;
 let op = "";
 let display = document.getElementById("display");
 let activeOp = false;
+let activeEqual = false;
 const numberButtons = document.querySelectorAll('.number');
 const operatorButtons = document.querySelectorAll('.operator');
 
@@ -16,6 +17,27 @@ operatorButtons.forEach(button => {
     button.addEventListener('click', function(event) {
         operatorPress(button.id)
     })
+});
+
+const equals = document.getElementById("equals");
+equals.addEventListener('click', function(event){
+    evaluate();
+    activeEqual = true;
+});
+
+const clear = document.getElementById("clear");
+clear.addEventListener('click', function(event){
+    clearAll();
+});
+
+const neg = document.getElementById("neg");
+neg.addEventListener('click', function(event){
+    negative();
+});
+
+const percent = document.getElementById("percent");
+percent.addEventListener('click', function(event){
+    alert("you got me I didn't add this functionality yet :)");
 });
 
 function add(x, y){
@@ -44,18 +66,75 @@ function operate(operator, x, y){
             return multiply(x, y);
         case "/":
             if(y == 0){
-                return "Cannot divite by 0."
+                a = 0;
+                b = 0;
+                op = "";
+                return "Cannot divide by 0."
             }
             return divide(x, y);
     }
 }
 
 function numberPress(num){
-    if(display.textContent == 0){
+    if(display.textContent == 0 || display.textContent == "Cannot divide by 0."){
         display.textContent = ""
     }
-    
+
+    if(activeOp){
+        display.textContent = "";
+        a = b;
+        activeOp = false;
+    }
+
+    if(b == null && a != null){
+        a = null;
+        display.textContent = ""
+    }
+
+    if(activeEqual){
+        a = null;
+        b = null;
+        op = "";
+        display.textContent = "";
+        activeEqual = false;
+    }
+
     display.textContent = display.textContent + num;
-    a = parseFloat(display.textContent)
-    console.log(a)
+    b = parseFloat(display.textContent)
+}
+
+function operatorPress(operator){
+    if(a == 0 && b == 0){
+        return;
+    }else if(a != 0 && b != 0){
+        evaluate();
+    }
+
+    op = operator;
+    activeOp = true;
+    activeEqual = false;
+}
+
+function evaluate(){
+    if(a == null || b == null || op == ""){
+        return
+    }
+
+    b = operate(op, a, b);
+    a = null;
+    op = "";
+
+    display.textContent = b;
+}
+
+function negative(){
+    b = b * -1;
+    display.textContent = b;
+}
+
+function clearAll(){
+    a = null;
+    b = null;
+    op = "";
+    display.textContent = "0";
 }
